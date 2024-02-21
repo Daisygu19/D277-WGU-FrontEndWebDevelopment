169f25acf5f4a54883d4e2f7344c6692e76299af
Daisygu19 <dgonzalez@appdev.yearup.org>
2024-02-21 14:18:03 -0500

    Added all files for submission

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// code for confirmation of second email
+
+document.addEventListener('DOMContentLoaded', function () {
+  var email = document.getElementById('email');
+  var confirmEmail = document.getElementById('confirmEmail');
+
+  function checkEmailMatch() {
+      
+      if (email.value !== confirmEmail.value) {
+          confirmEmail.setCustomValidity('Email addresses must match.');
+
+          
+          confirmEmail.style.borderColor = 'red';
+
+          
+          alert('Email addresses do not match. Please enter matching email addresses.');
+      } else {
+          confirmEmail.setCustomValidity('');
+
+          
+          confirmEmail.style.borderColor = 'green';
+
+          
+          alert('Email addresses match. Verification successful.');
+
+
+      }
+  }
+
+  email.addEventListener('change', checkEmailMatch);
+  confirmEmail.addEventListener('change', checkEmailMatch);
+});
